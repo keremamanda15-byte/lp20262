43d4ec721ed723afc412c6d0ea995adca1c21e37
keremamanda15-byte <keremamanda15@gmail.com>
2026-02-11 15:23:15 +0000

    resolvendo exercicios

---
+++ exercicio.py
@@ -0,0 +1,44 @@
+def q1():
+    nome="kerem amanda"
+    print(nome)
+
+
+q1()
+
+# Faça um programa que imprima o produto dos valores 30 e 27.
+
+def q2():
+    def q2():
+        print (30*27)
+
+        q2()
+
+
+# Faça um programa que imprima a média aritmética entre os números 5, 8, 12.
+
+def q3():
+    media = (5+8+12)/3
+    print (f'(5+8+12)/3' = {media}')
+
+    q3()
+    
+# faça programa que leia e imprima um número inteiro.
+def q4():
+    numero = int(input(Digite um numero inteiro: ')
+    print(f'voce digitou: {numero}')
+
+ #5. Faça um programa que leia dois números reais e os imprima.
+ def q5():
+    num1 =float('digite um numero real:'))
+    num2 = float(digite outro numero:'))
+    print(f'os numeros digitados foram: {num1:. 1f} e {num2:.2f}')
+
+    q5()
+    
+    #6. Faça um programa que leia um número inteiro e imprima o seu
+#   antecessor e o seu sucessor.
+
+
+
+
+
