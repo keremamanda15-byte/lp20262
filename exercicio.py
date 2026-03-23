2b1ccce490a794576d293d7ce58fa3754fe43fec
keremamanda15-byte <keremamanda15@gmail.com>
2026-03-22 21:04:28 -0400

    Implement multiple Python exercises with functions
    Added Python exercises with functions for various tasks.

--- exercicio.py
+++ exercicio.py
@@ -51,7 +51,257 @@ telefone= input("Digite seu telefone:")
 print(f"\nNome:{nome}|endereço:{endereço}\nTelefone}")
 
 def questao-8
- 
+ # LISTA 1 - EXERCÍCIOS DE PYTHON
+
+Obs: 
+    
+    \n - Quebra de Texto
+    :.2f - Arredonda o número real em duas casas.
+    .title() - Transforma as palavras em títulos (Letra inicial em maiúscula).
+    .strip() - Retira os espaços nas extremidades de uma string (texto).
+    # - Usado para comentar uma linha do código, essa linha quando comentada não é executada no programa.
+    questao_1,2,3...() - Para rodar uma questão específica, retire a # da frente da questão para funcionar.
+
+# 1. Faça um programa que imprima o seu nome.
+
+def questao_1():
+    nome = input("Digite seu nome: ").title().strip()
+    print (f"Nome: {nome}")
+
+# questao_1()
+
+# 2. Faça um programa que imprima o produto dos valores 30 e 27.
+
+def questao_2():
+    print(f"Produto dos valores 30 e 27: {30*27}")
+
+# questao_2()
+
+# 3. Faça um programa que imprima a média aritmética entre os números 5, 8, 12.
+
+def questao_3():
+    soma = 5 + 8 + 12
+    media = soma/3
+
+    print(f"Média entre 5, 8, 12: {media:.2f}")
+
+# questao_3()
+
+# 4. Faça um programa que leia e imprima um número inteiro.
+
+def questao_4():
+    num = int(input("Digite um número inteiro: "))
+    print(f"Número inteiro digitado: {num}")
+
+# questao_4()
+
+# 5. Faça um programa que leia dois números reais e os imprima.
+
+def questao_5():
+    num1 = float(input("Digite o 1º número real: "))
+    num2 = float(input("Digite o 2º número real: "))
+
+    print (f"\nNúmeros reais digitados: {num1} e {num2}")
+
+# questao_5()
+
+# 6. Faça um programa que leia um número inteiro e imprima o seu antecessor e o seu sucessor.
+
+def questao_6():
+    num = int(input("Digite um número inteiro: "))
+    print (f"\nAntecessor de {num}: {num - 1}")
+    print (f"Sucessor de {num}: {num + 1}")
+
+# questao_6()
+
+# 7. Faça um programa que leia o nome o endereço e o telefone de um cliente e ao final, imprima esses dados.
+
+def questao_7():
+    nome = input("Digite seu nome: ").title().strip()
+    endereco = input("Digite seu endereço: ")
+    telefone = input("Digite seu telefone: ")
+
+    print (f"\nNome: {nome}\nEndereço: {endereco}\nTelefone: {telefone}")
+
+# questao_7()
+
+# 8. Faça um programa que leia dois números inteiros e imprima a subtração deles.
+
+def questao_8():
+    num1 = int(input("Digite o 1º número: "))
+    num2 = int(input("Digite o 2º número: "))
+
+    print (f"\nSubtração de {num1} e {num2}: {num1 - num2}")
+
+# questao_8()
+
+# 9. Faça um programa que leia um número real e imprima ¼ deste número.
+
+def questao_9():
+    num = float(input("Digite um número real: "))
+    
+    print (f"1/4 do número {num}: {num/4}")
+
+# questao_9()
+
+# 10. Faça um programa que leia três números reais e calcule a média aritmética destes números.
+# Ao final, o programa deve imprimir o resultado do cálculo.
+
+def questao_10():
+    num1 = float(input("Digite o 1º número real: "))
+    num2 = float(input("Digite o 2º número real: "))
+    num3 = float(input("Digite o 3º número real: "))
+
+    soma = num1 + num2 + num3
+    media = soma/3
+
+    print(f"\nMédia aritmética dos números {num1}, {num2}, {num3}: {media:.2f}")
+
+# questao_10()
+
+# 11. Faça um programa que leia dois números reais e calcule as quatro operações básicas entre estes dois números, adição,
+# subtração,multiplicação e divisão. Ao final, o programa deve imprimir os resultados dos cálculos.
+
+def questao_11():
+    num1 = float(input("Digite o 1º número real: "))
+    num2 = float(input("Digite o 2º número real: "))
+
+    print (f"\nAdição: {num1 + num2"}
+    print (f"Subtração: {num1 - num2}")
+    print (f"Multiplicação: {num1 * num2}")
+    print (f"Divisão: {num1 / num2:.2f}")
+
+# questao_11()
+
+# 12. Faça um programa que leia um número real e calcule o quadrado deste número. Ao final, o programa deve imprimir o resultado do cálculo.
+
+def questao_12():
+    num = float(input("Digite um número real: "))
+
+    print(f"\nQuadrado do valor {num}: {num*num}")
+
+# questao_12()
+
+# 13. Faça um programa que leia o saldo de uma conta poupança e imprima o novo saldo, considerando um reajuste de 2%.
+
+def questao_13():
+    saldo = float(input("Informe o saldo da conta poupança: "))
+    novo_saldo = saldo * 1.02
+
+    print(f"\nNovo saldo com reajuste de 2%: {novo_saldo:.2f}")
+
+# questao_13()
+
+# 14. Faça um programa que leia a base e a altura de um retângulo e imprima o perímetro (base*2 + altura*2) e a área (base * altura).    
+
+def questao_14():
+    base = float(input("Informe a base do retãngulo: "))
+    altura = float(input("Informe a altura do retângulo: "))
+
+    perimetro = (base*2 + altura*2)
+    area = (base * altura)
+
+    print(f"\nPerímetro do retângulo: {perimetro:.2f} m")
+    print(f"Área do retângulo: {area:.2f} m²")
+
+# questao_14()
+
+# 15. Faça um programa que leia o valor de um produto, o percentual do desconto desejado e imprima o valor do desconto e o valor do produto subtraindo o desconto.
+
+def questao_15():
+    produto = float(input("Informe o valor do produto: "))
+    percentual = float(input("Informe o percentual do desconto desejado: "))
+
+    desconto = produto * (percentual/100)
+    valor_final = produto - desconto
+
+    print(f"\nValor do desconto: R${desconto:.2f}")
+    print(f"Valor do final do produto: R${valor_final:.2f}")
+
+# questao_15()
+
+# 16. Faça um programa que calcule o reajuste do salário de um funcionário. Para isso, o programa deverá ler o salário atual do funcionário e ler o percentual de reajuste.
+# Ao final imprimir o valor do novo salário.
+
+def questao_16():
+    salario_atual = float(input("Informe o salário atual: "))
+    percentual_reajuste = float(input("Informe o percentual de reajuste: "))
+
+    novo_salario = salario_atual + (salario_atual * (percentual_reajuste/100))
+    print(f"\nNovo salário com o reajuste: R${novo_salario:.2f}")
+
+# questao_16()
+
+# 17. Faça um programa que calcule a conversão entre graus centígrados e Fahrenheit.
+# Para isso, leia o valor em centígrados e calcule com base na fórmula a seguir.
+# Após calcular o programa deve imprimir o resultado da conversão. 
+# F = (9 x C + 160) / 5
+
+def questao_17():
+    centigrados = float(input("Informe a temperatura em graus centígrados: "))
+    fahrenheit = (9 * centigrados + 160) / 5
+
+    print(f"\nConversão de graus centígrados para Fahrenheit: {fahrenheit:.2f} °F")
+
+# questao_17()
+
+# 18. Faça um programa que calcule a quantidade de litros de combustível consumidos em uma viagem, sabendo-se que o carro tem autonomia de
+# 12 km por litro de combustível. O programa deverá ler o tempo decorrido na viagem e a velocidade média e aplicar as fórmulas: 
+# D = T x V       L = D / 12
+
+# Em que:
+# 
+# • D = Distância percorrida
+# • T = Tempo
+# • V = Velocidade média
+# • L = Litros de combustível consumidos
+
+# Ao final, o programa deverá imprimir a distância percorrida e a quantidade de litros consumidos na viagem.
+
+def questao_18():
+    t = float(input("Informe o tempo decorrido na viagem: "))
+    v = float(input("Informe a velocidade média: "))
+
+    d = t * v
+    l = d / 12
+
+    print(f"\nDistância percorrida na viagem: {d} m")
+    print(f"Quantidade de litros consumidos na viagem: {l:.2f} l")
+
+# questao_18()
+
+# 19. Faça um programa que calcule o valor de uma prestação em atraso.
+# Para isso, o programa deve ler o valor da prestação vencida, a taxa periódica de juros e o período de atraso.
+# Ao final, o programa deve imprimir o valor da prestação atrasada, o período
+# de atraso, os juros que serão cobrados pelo período de atraso, o
+# valor da prestação acrescido dos juros. Considere juros simples.
+
+def questao_19():
+    prestacao_vencida = float(input("Informe o valor da prestação em atraso: "))
+    taxa_periodica_juros = float(input("Informe a taxa periódica de juros: "))
+    periodo_atraso = float(input("Informe o período de atraso: "))
+
+    juros = prestacao_vencida * (taxa_periodica_juros/100) * periodo_atraso
+    valor_final = prestacao_vencida + juros
+
+    print(f"\nValor da prestação atrasada: R${prestacao_vencida:.2f}")
+    print(f"Período de atraso: {periodo_atraso}")
+    print(f"Juros cobrados: R${juros:.2f}")
+    print(f"Valor total da prestação com juros: R${valor_final:.2f}")
+
+# questao_19()
+
+# 20. Faça um programa que efetue a apresentação do valor da conversão em real (R$) de um valor lido em dólar (US$).
+# Para isso, será necessário também ler o valor da cotação do dólar.
+
+def questao_20():
+    dolar = float(input("Informe o valor em dólar (US$): "))
+    real = dolar * 5.20
+
+    print(f"\nDólar (US$) convertido para real (R$): R${real:.2f}")
+
+# questao_20()
+
 
 
 
